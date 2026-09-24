ebc532829e4c3be16869feb2f64797887bba6a27
Stefan Ziegler <edi.gonzales@gmail.com>
2026-09-24 21:08:16 +0200

    Make raster progress path assertion platform neutral

--- raster/raster-values/src/test/java/ch/so/agi/hop/raster/values/RasterWriteProgressTest.java
+++ raster/raster-values/src/test/java/ch/so/agi/hop/raster/values/RasterWriteProgressTest.java
@@ -11,7 +11,8 @@ class RasterWriteProgressTest {
   void emitsBasicMessagesAtMostOncePerTenPercentMilestone() {
     var messages = new ArrayList<String>();
     var progress = new RasterWriteProgress(messages::add);
-    progress.started(Path.of("/tmp/output.tif"), "Deflate");
+    var output = Path.of("/tmp/output.tif");
+    progress.started(output, "Deflate");
     for (int value : new int[] {0, 9, 10, 10, 19, 20, 33, 99, 100, 100, 150})
       progress.report(value);
     progress.completed();
@@ -20,7 +21,9 @@ class RasterWriteProgressTest {
     assertThat(messages.subList(1, 6))
         .extracting(message -> message.substring(message.lastIndexOf(' ') + 1))
         .containsExactly("10%", "20%", "30%", "90%", "100%");
-    assertThat(messages.get(0)).contains("writing /tmp/output.tif with Deflate compression");
-    assertThat(messages.get(6)).contains("wrote /tmp/output.tif with Deflate compression in ");
+    assertThat(messages.get(0))
+        .contains("writing " + output + " with Deflate compression");
+    assertThat(messages.get(6))
+        .contains("wrote " + output + " with Deflate compression in ");
   }
 }
